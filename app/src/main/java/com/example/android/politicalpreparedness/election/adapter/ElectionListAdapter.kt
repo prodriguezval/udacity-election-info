bf338dc44b1981bdb4b097cb5e58ef5e2d4e785a
Pablo Rodriguez <pablo.rodriguez@thoughtworks.com>
2022-03-25 18:46:56 -0300

    building app is working

--- app/src/main/java/com/example/android/politicalpreparedness/election/adapter/ElectionListAdapter.kt
+++ app/src/main/java/com/example/android/politicalpreparedness/election/adapter/ElectionListAdapter.kt
@@ -1,23 +1,48 @@
 package com.example.android.politicalpreparedness.election.adapter
 
+import android.view.LayoutInflater
 import android.view.ViewGroup
+import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.ListAdapter
-import com.example.android.politicalpreparedness.databinding.ViewholderElectionBinding
+import androidx.recyclerview.widget.RecyclerView
+import com.example.android.politicalpreparedness.databinding.ElectionListItemBinding
 import com.example.android.politicalpreparedness.network.models.Election
 
-class ElectionListAdapter(private val clickListener: ElectionListener): ListAdapter<Election, ElectionViewHolder>(ElectionDiffCallback()) {
+class ElectionListAdapter(private val clickListener: ElectionListener) :
+    ListAdapter<Election, ElectionViewHolder>(ElectionDiffCallback()) {
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ElectionViewHolder {
         return ElectionViewHolder.from(parent)
     }
 
-    //TODO: Bind ViewHolder
-
-    //TODO: Add companion object to inflate ViewHolder (from)
+    override fun onBindViewHolder(holder: ElectionViewHolder, position: Int) {
+        val item = getItem(position)
+        holder.bind(item, clickListener)
+    }
 }
 
-//TODO: Create ElectionViewHolder
+class ElectionViewHolder private constructor(private val binding: ElectionListItemBinding) :
+    RecyclerView.ViewHolder(binding.root) {
+    fun bind(item: Election, clickListener: ElectionListener) {
+        binding.executePendingBindings()
+    }
 
-//TODO: Create ElectionDiffCallback
+    companion object {
+        fun from(parent: ViewGroup): ElectionViewHolder {
+            val layoutInflater = LayoutInflater.from(parent.context)
+            val binding = ElectionListItemBinding.inflate(layoutInflater, parent, false)
+            return ElectionViewHolder(binding)
+        }
+    }
+}
 
-//TODO: Create ElectionListener
+class ElectionDiffCallback : DiffUtil.ItemCallback<Election>() {
+    override fun areItemsTheSame(oldItem: Election, newItem: Election) =
+        oldItem.id == newItem.id
+
+    override fun areContentsTheSame(oldItem: Election, newItem: Election) = oldItem == newItem
+}
+
+class ElectionListener(private val clickListener: (Election) -> Unit) {
+    fun onClick(data: Election) = clickListener(data)
+}
--- app/src/main/java/com/example/android/politicalpreparedness/representative/adapter/RepresentativeListAdapter.kt
+++ app/src/main/java/com/example/android/politicalpreparedness/representative/adapter/RepresentativeListAdapter.kt
@@ -10,8 +10,7 @@ import android.widget.ImageView
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
-import com.example.android.politicalpreparedness.R
-import com.example.android.politicalpreparedness.databinding.ViewholderRepresentativeBinding
+import com.example.android.politicalpreparedness.databinding.RepresentativeListItemBinding
 import com.example.android.politicalpreparedness.network.models.Channel
 import com.example.android.politicalpreparedness.representative.model.Representative
 
@@ -27,11 +26,12 @@ class RepresentativeListAdapter: ListAdapter<Representative, RepresentativeViewH
     }
 }
 
-class RepresentativeViewHolder(val binding: ViewholderRepresentativeBinding): RecyclerView.ViewHolder(binding.root) {
+class RepresentativeViewHolder(val binding: RepresentativeListItemBinding) :
+    RecyclerView.ViewHolder(binding.root) {
 
     fun bind(item: Representative) {
-        binding.representative = item
-        binding.representativePhoto.setImageResource(R.drawable.ic_profile)
+//        binding.representative = item
+//        binding.representativePhoto.setImageResource(R.drawable.ic_profile)
 
         //TODO: Show social links ** Hint: Use provided helper methods
         //TODO: Show www link ** Hint: Use provided helper methods
@@ -39,18 +39,24 @@ class RepresentativeViewHolder(val binding: ViewholderRepresentativeBinding): Re
         binding.executePendingBindings()
     }
 
-    //TODO: Add companion object to inflate ViewHolder (from)
+    companion object {
+        fun from(parent: ViewGroup): RepresentativeViewHolder {
+            val layoutInflater = LayoutInflater.from(parent.context)
+            val binding = RepresentativeListItemBinding.inflate(layoutInflater, parent, false)
+            return RepresentativeViewHolder(binding)
+        }
+    }
 
     private fun showSocialLinks(channels: List<Channel>) {
         val facebookUrl = getFacebookUrl(channels)
-        if (!facebookUrl.isNullOrBlank()) { enableLink(binding.facebookIcon, facebookUrl) }
+        //if (!facebookUrl.isNullOrBlank()) { enableLink(binding.facebookIcon, facebookUrl) }
 
         val twitterUrl = getTwitterUrl(channels)
-        if (!twitterUrl.isNullOrBlank()) { enableLink(binding.twitterIcon, twitterUrl) }
+        //if (!twitterUrl.isNullOrBlank()) { enableLink(binding.twitterIcon, twitterUrl) }
     }
 
     private fun showWWWLinks(urls: List<String>) {
-        enableLink(binding.wwwIcon, urls.first())
+        //enableLink(binding.wwwIcon, urls.first())
     }
 
     private fun getFacebookUrl(channels: List<Channel>): String? {
@@ -78,6 +84,14 @@ class RepresentativeViewHolder(val binding: ViewholderRepresentativeBinding): Re
 
 }
 
-//TODO: Create RepresentativeDiffCallback
+class RepresentativeDiffCallback : DiffUtil.ItemCallback<Representative>() {
+    override fun areItemsTheSame(oldItem: Representative, newItem: Representative) =
+        oldItem.official.name == newItem.official.name
 
-//TODO: Create RepresentativeListener
+    override fun areContentsTheSame(oldItem: Representative, newItem: Representative) =
+        oldItem == newItem
+}
+
+class RepresentativeListener(private val clickListener: (Representative) -> Unit) {
+    fun onClick(data: Representative) = clickListener(data)
+}
