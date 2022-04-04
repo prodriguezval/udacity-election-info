3510e7102f664b397c37e932393bba2bf77ba868
Pablo Rodriguez <pablo.rodriguez@thoughtworks.com>
2022-04-04 11:07:29 -0400

    fix address stateHandle error

--- app/src/main/java/com/example/android/politicalpreparedness/representative/RepresentativeViewModel.kt
+++ app/src/main/java/com/example/android/politicalpreparedness/representative/RepresentativeViewModel.kt
@@ -15,12 +15,19 @@ class RepresentativeViewModel(private val app: Application, private val uiState:
     ViewModel() {
     companion object {
         private const val TAG = "RepresentativeViewModel"
+        private const val STATE_ADDRESS = "address"
+        private const val STATE_REPRESENTATIVES = "representatives"
+        private const val STATE_SELECTED_US_STATE = "state"
+        private const val STATE_ADDRESS_LINE_1 = "address_line_1"
+        private const val STATE_ADDRESS_LINE_2 = "address_line_2"
+        private const val STATE_ADDRESS_CITY = "address_city"
+        private const val STATE_ADDRESS_ZIP_CODE = "address_zip"
     }
 
     private val repository = RepresentativeRepository(CivicsApi)
 
     private val _address = uiState.getLiveData(
-        "address",
+        STATE_ADDRESS,
         Address("", "", "", "New York", "")
     )
     val address: LiveData<Address>
@@ -31,11 +38,16 @@ class RepresentativeViewModel(private val app: Application, private val uiState:
         get() = _states
 
     private val _representatives =
-        uiState.getLiveData<List<Representative>>("representatives", emptyList())
+        uiState.getLiveData<List<Representative>>(STATE_REPRESENTATIVES, emptyList())
     val representatives: LiveData<List<Representative>>
         get() = _representatives
 
-    val selectedStateIndex = uiState.getLiveData("state", 0)
+    val selectedStateIndex = uiState.getLiveData(STATE_SELECTED_US_STATE, 0)
+
+    val addressLine1 = uiState.getLiveData(STATE_ADDRESS_LINE_1, "")
+    val addressLine2 = uiState.getLiveData(STATE_ADDRESS_LINE_2, "")
+    val city = uiState.getLiveData(STATE_ADDRESS_CITY, "")
+    val zipCode = uiState.getLiveData(STATE_ADDRESS_ZIP_CODE, "")
 
     init {
         _states.value = app.resources.getStringArray(R.array.states).toList()
@@ -48,7 +60,7 @@ class RepresentativeViewModel(private val app: Application, private val uiState:
                 val addressStr = address.value!!.toFormattedString()
                 repository.getRepresentatives(addressStr).collect {
                     _representatives.value = it
-                    uiState.set("representatives", it)
+                    uiState.set(STATE_REPRESENTATIVES, it)
                 }
             } catch (e: Exception) {
                 Log.e(TAG, e.message, e)
@@ -69,12 +81,43 @@ class RepresentativeViewModel(private val app: Application, private val uiState:
         Log.i(TAG, "state index $stateIndex for ${address.state}")
         if (stateIndex != null && stateIndex >= 0) {
             selectedStateIndex.value = stateIndex!!
-            uiState.set("state", stateIndex)
-            _address.value = address
-            uiState.set("address", address)
+            uiState.set(STATE_SELECTED_US_STATE, stateIndex)
+            uiState.set(STATE_ADDRESS_LINE_1, address.line1)
+            uiState.set(STATE_ADDRESS_LINE_2, address.line2)
+            uiState.set(STATE_ADDRESS_CITY, address.city)
+            uiState.set(STATE_ADDRESS_ZIP_CODE, address.zip)
+            uiState.set(STATE_SELECTED_US_STATE, stateIndex)
             refreshRepresentatives()
         } else {
             Log.e(TAG, "Current location isn't US")
         }
+    }
+
+    fun updateAddressLine1(s: CharSequence, start: Int, before: Int, count: Int) {
+        val text = s.toString()
+        addressLine1.value = text
+        uiState.set(STATE_ADDRESS_LINE_1, text)
+    }
+
+    fun updateAddressLine2(s: CharSequence, start: Int, before: Int, count: Int) {
+        val text = s.toString()
+        addressLine2.value = text
+        uiState.set(STATE_ADDRESS_LINE_2, text)
+    }
+
+    fun updateCity(s: CharSequence, start: Int, before: Int, count: Int) {
+        val text = s.toString()
+        city.value = text
+        uiState.set(STATE_ADDRESS_CITY, text)
+    }
+
+    fun updateZip(s: CharSequence, start: Int, before: Int, count: Int) {
+        val text = s.toString()
+        zipCode.value = text
+        uiState.set(STATE_ADDRESS_ZIP_CODE, text)
+    }
+
+    fun updateUSState() {
+
     }
 }
